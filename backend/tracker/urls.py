4f5d65f0b17378dff4f38e3503d0a6b8da46174a
Maciej Lorenc <lorencmaciek@gmail.com>
2018-01-21 23:40:07 +0100

    API v4

--- backend/tracker/urls.py
+++ backend/tracker/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('api/v2/readings/', views.reading_list, name="reading_list"),
-    path('api/v2/readings/<int:pk>/', views.reading_detail, name="reading_detail"),
+    path('api/v4/readings/', views.ReadingListV4.as_view(), name="reading_list_v4"),
+    path('api/v4/readings/<int:pk>/', views.ReadingDetailV4.as_view(), name="reading_detail_v4"),
+    path('api/v3/readings/', views.ReadingListV3.as_view(), name="reading_list_v3"),
+    path('api/v3/readings/<int:pk>/', views.ReadingDetailV3.as_view(), name="reading_detail_v3"),
+    path('api/v2/readings/', views.reading_list_v2, name="reading_list_v2"),
+    path('api/v2/readings/<int:pk>/', views.reading_detail_v2, name="reading_detail_v2"),
     path('api/v1/readings/', views.reading_list_v1, name="reading_list_v1"),
     path('api/v1/readings/<int:pk>/',
          views.reading_detail_v1, name="reading_detail_v1"),
--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -1,6 +1,13 @@
+from django.http import Http404
+from rest_framework import generics, mixins
+
+# API v3
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
+# API v2
+from rest_framework.decorators import api_view 
 
 # API v1
 from django.http import HttpResponse, JsonResponse
@@ -17,8 +24,78 @@ def index(request):
     return HttpResponse('tracker index works!')
 
 
+class ReadingListV4(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Reading.objects.all()
+    serializer_class = ReadingSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, *kwargs)
+
+
+class ReadingDetailV4(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Reading.objects.all()
+    serializer_class = ReadingSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class ReadingListV3(APIView):
+    def get(self, request, format=None):
+        readings = Reading.objects.all()
+        serializer = ReadingSerializer(readings, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ReadingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ReadingDetailV3(APIView):
+    def get_object(self, pk):
+        try:
+            return Reading.objects.get(pk=pk)
+        except Reading.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        reading = self.get_object(pk)
+        serializer = ReadingSerializer(reading)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        reading = self.get_object(pk)
+        serializer = ReadingSerializer(reading, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        reading = self.get_object(pk)
+        reading.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 @api_view(['GET', 'POST'])
-def reading_list(request, format=None):
+def reading_list_v2(request, format=None):
     if request.method == 'GET':
         readings = Reading.objects.all()
         serializer = ReadingSerializer(readings, many=True)
@@ -33,7 +110,7 @@ def reading_list(request, format=None):
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def reading_detail(request, pk, format=None):
+def reading_detail_v2(request, pk, format=None):
     try:
         reading = Reading.objects.get(pk=pk)
     except Reading.DoesNotExist:
