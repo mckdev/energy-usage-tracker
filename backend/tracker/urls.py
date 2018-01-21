f18cd0531a457ee13a6315a670a33a843d794f04
Maciej Lorenc <lorencmaciek@gmail.com>
2018-01-21 23:22:24 +0100

    API v2

--- backend/tracker/urls.py
+++ backend/tracker/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('api/v1/readings/', views.reading_list, name="reading_list"),
-    path('api/v1/readings/<int:pk>/', views.reading_detail, name="reading_detail"),
+    path('api/v2/readings/', views.reading_list, name="reading_list"),
+    path('api/v2/readings/<int:pk>/', views.reading_detail, name="reading_detail"),
+    path('api/v1/readings/', views.reading_list_v1, name="reading_list_v1"),
+    path('api/v1/readings/<int:pk>/',
+         views.reading_detail_v1, name="reading_detail_v1"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -1,7 +1,14 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# API v1
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+
+# Models and serializers
 from .models import Reading
 from .serializers import ReadingSerializer
 
@@ -10,8 +17,46 @@ def index(request):
     return HttpResponse('tracker index works!')
 
 
+@api_view(['GET', 'POST'])
+def reading_list(request, format=None):
+    if request.method == 'GET':
+        readings = Reading.objects.all()
+        serializer = ReadingSerializer(readings, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ReadingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def reading_detail(request, pk, format=None):
+    try:
+        reading = Reading.objects.get(pk=pk)
+    except Reading.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ReadingSerializer(reading)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ReadingSerializer(reading, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        reading.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 @csrf_exempt
-def reading_list(request):
+def reading_list_v1(request):
     if request.method == 'GET':
         readings = Reading.objects.all()
         serializer = ReadingSerializer(readings, many=True)
@@ -19,7 +64,7 @@ def reading_list(request):
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
+        serializer = ReadingSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
@@ -27,7 +72,7 @@ def reading_list(request):
 
 
 @csrf_exempt
-def reading_detail(request, pk):
+def reading_detail_v1(request, pk):
     try:
         reading = Reading.objects.get(pk=pk)
     except Reading.DoesNotExist:
@@ -39,7 +84,7 @@ def reading_detail(request, pk):
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
-        serializer = SnippetSerializer(reading, data=data)
+        serializer = ReadingSerializer(reading, data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
