87bd649bb000280b4be20be278ad5b6694211b3b
Maciej Lorenc <lorencmaciek@gmail.com>
2018-01-22 01:20:15 +0100

    Refactored code

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('tracker.urls')),
+    path('api/', include('tracker.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- backend/tracker/urls.py
+++ backend/tracker/urls.py
@@ -8,26 +8,27 @@ router.register(r'readings', views.ReadingViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    path(r'api/v6/', include(router.urls)),
-    path('api/v5/', views.api_root, name="api-root"),
-    path('api/v5/users/', views.UserList.as_view(), name="user-list"),
-    path('api/v5/users/<int:pk>/', views.UserDetail.as_view(), name="user-detail"),
-    path('api/v5/readings/', views.ReadingList.as_view(), name="reading-list"),
-    path('api/v5/readings/<int:pk>/',
-         views.ReadingDetail.as_view(), name="reading-detail"),
+    path('v6/', include(router.urls)),
     # Old API versions
-    path('api/v4/readings/', views.ReadingList.as_view(), name="reading-list_v4"),
-    path('api/v4/readings/<int:pk>/',
+    path('v5/', views.api_root, name="api-root"),
+    path('v5/users/', views.UserList.as_view(), name="user-list"),
+    path('v5/users/<int:pk>/', views.UserDetail.as_view(), name="user-detail"),
+    path('v5/readings/', views.ReadingList.as_view(), name="reading-list"),
+    path('v5/readings/<int:pk>/',
+         views.ReadingDetail.as_view(), name="reading-detail"),
+    path('v4/readings/', views.ReadingList.as_view(), name="reading-list_v4"),
+    path('v4/readings/<int:pk>/',
          views.ReadingDetailV4.as_view(), name="reading-detail_v4"),
-    path('api/v3/readings/', views.ReadingListV3.as_view(), name="reading-list_v3"),
-    path('api/v3/readings/<int:pk>/',
+    path('v3/readings/', views.ReadingListV3.as_view(), name="reading-list_v3"),
+    path('v3/readings/<int:pk>/',
          views.ReadingDetailV3.as_view(), name="reading-detail_v3"),
-    path('api/v2/readings/', views.reading_list_v2, name="reading-list_v2"),
-    path('api/v2/readings/<int:pk>/',
+    path('v2/readings/', views.reading_list_v2, name="reading-list_v2"),
+    path('v2/readings/<int:pk>/',
          views.reading_detail_v2, name="reading-detail_v2"),
-    path('api/v1/readings/', views.reading_list_v1, name="reading-list_v1"),
-    path('api/v1/readings/<int:pk>/',
+    path('v1/readings/', views.reading_list_v1, name="reading-list_v1"),
+    path('v1/readings/<int:pk>/',
          views.reading_detail_v1, name="reading-detail_v1"),
 ]
 
+# This breaks router urls, but is useful for older API versions
 # urlpatterns = format_suffix_patterns(urlpatterns)
