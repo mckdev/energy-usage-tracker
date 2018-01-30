8f99e2d1a962064515e9dd1df7584aa12020ea6d
Maciej Lorenc <lorencmaciek@gmail.com>
2018-01-30 17:44:07 +0100

    Secret key is generated on the server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,9 +2,27 @@ import os
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = '9jsr8vl5&=53(bjg^4nb#%05-6m*+ptb0x*-@xn&&f=+5ni)ep'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+
+# Secret Key
+
+try:
+    SECRET_KEY
+except NameError:
+    SECRET_FILE = os.path.join(BASE_DIR, 'secret.txt')
+    try:
+        SECRET_KEY = open(SECRET_FILE).read().strip()
+    except IOError:
+        try:
+            import random
+            SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
+            secret = open(SECRET_FILE, 'w')
+            secret.write(SECRET_KEY)
+            secret.close()
+        except IOError:
+            Exception('Please create a %s file with random characters \
+            to generate your secret key!' % SECRET_FILE)
 
 
 # Application definition
@@ -100,7 +118,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static-root')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-]   
+]
 
 
 # API
