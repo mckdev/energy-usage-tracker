baf07af09f96a59e2151baaaadc9fcba8a2c158b
Maciej Lorenc <lorencmaciek@gmail.com>
2018-01-30 19:43:42 +0100

    Date can now be edited from admin

--- backend/tracker/models.py
+++ backend/tracker/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Reading(models.Model):
     user = models.ForeignKey(
         'auth.User', related_name='readings', on_delete=models.CASCADE)
-    date = models.DateTimeField('reading date', auto_now_add=True)
+    date = models.DateTimeField('reading date', blank=True)
     value = models.FloatField()
 
     class Meta:
         ordering = ('date',)
+
+    def save(self, *args, **kwargs):
+        if not self.date:
+            self.date = timezone.now()
+        super().save(*args, **kwargs)
